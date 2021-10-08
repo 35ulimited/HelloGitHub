b2a1cee0f7e20c50193631400b7b6682b4910c84
wenfang.tang <niky313@163.com>
2021-10-08 11:20:27 +0800

    asdf

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println('hello worldaaabgb');
+        System.out.println('hello worldaaabgfb');
     }
 
 }
